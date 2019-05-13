c563a863a00ec4dfaa001f9f7652d1c3bb73c575
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-13 15:11:58 -0400

    imported header into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddPet from './components/AddPet/AddPet'
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
     <h1>PAWSOME</h1>
+    <Header />
     <AddPet />
     </div>
   );
