b23738aa2f96c704004f8fac19d382db9f2bb6fe
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-13 16:50:36 -0400

    added main

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import AddPet from './components/AddPet/AddPet';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <h1>PAWSOME</h1>
     <Header />
     <AddPet />
+    <Main />
     <Footer />
     </div>
   );
