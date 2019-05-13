0afe03abb3448a707f13434c12804d68c51b3d33
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-13 15:02:28 -0400

    fixed typo

--- client/src/components/AddPet/AddPet.js
+++ client/src/components/AddPet/AddPet.js
@@ -15,12 +15,12 @@ class AddPet extends Component {
             image: null
         }
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.buind(this);
+        this.handleSubmit = this.handleSubmit.bind(this); // bind it, don't buind it.
     }
 
     handleSubmit = (e) => {
 
-            
+
     }
     handleChange = (e) => {
 
@@ -31,64 +31,64 @@ class AddPet extends Component {
             <div>
                 <form>
                     <label>Name:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Pet Name'
                         id='name'
                         name='name'
                     />
                     <label>Age:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Age'
                         id='age'
                         name='age'
                     />
                     <label>Species:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Species'
                         id='species'
                         name='species'
                     />
                     <label>Color:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Color'
                         id='color'
-                        name='color' 
+                        name='color'
                     />
                     <label>Gender:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Gender'
                         id='gender'
                         name='gender'
                     />
                     <label>Breed:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Breed'
                         id='breed'
                         name='breed'
                     />
                     <label>Medical:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Fixed'
                         id='medical'
                         name='medical'
                     />
                     <label>Bio:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Biography'
                         id='biography'
                         name='biography'
                     />
                     <label>Image:</label>
-                    <input 
-                        type='input' 
+                    <input
+                        type='input'
                         placeholder='Image File'
                         id='image'
                         name='image'
