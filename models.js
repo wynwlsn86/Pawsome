54d664325b81d69a1db502cc152ebf77ab34c369
Charles Korpics <korpx94@gmail.com>
2019-05-13 11:48:48 -0400

    one model complete

--- models.js
+++ models.js
@@ -23,6 +23,50 @@ const AuthUser = pawDb.define('authuser', {
     }
   })
 
+  const Pet = db.define('pets', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    species: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    gender: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    breed: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    birthday: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    age: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    fixed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+    },
+    bio: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    image: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+  })
+
+  const Visit = db.define('pets', {
+ 
+  })
+
+
   module.exports = {
     pawDb,
     AuthUser
