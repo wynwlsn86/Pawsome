6f87d19339a87bee1eb55876b8e941663041cc0a
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-15 19:59:56 -0400

    home page styling basically complete

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ollyrhae from '../../assets/ollyrhae.jpg'
+
 
 
 
@@ -7,20 +9,28 @@ function Home() {
     <div className="App">
     <div>
       <div class="Main">
-         <img src="https://via.placeholder.com/250" alt="pet photos" ALIGN="left"/>
-            <div>
+         <img src={ollyrhae} alt="pet photos" ALIGN="left"/>
+            <div class="About">
               <h1>
                 Pawsome Animal Rescue
               </h1>
                 <h4>
-                Pawsome Pet Adoptions" is based on Long Island, NY, it's purpose is to find
-animal loving, helpful, caring, and responsible fosters, adopters, friends,
-and forever homes for all pets in need. We stem from an up and coming rescue
-"Pawsome Animal Rescue" and we understand the desperate need to share each
+                Pawsome Pet Adoptions" is based on Long Island, NY. Its purpose is to find
+animal-loving, helpful, caring, and responsible fosters, adopters, friends,
+and forever homes for all pets in need.
+                </h4>
+                <h4>
+We stem from an up and coming rescue "Pawsome Animal Rescue" and we understand the desperate need to share each
 animal, their story, and give them the exposure they need to find the right
-family. Our goals are advocacy, outreach, education, rescue, and adoptions
+family.
+                </h4>
+                <h4>
+ Our goals are advocacy, outreach, education, rescue, and adoptions
 for ANY and ALL animals in need. We help people with pets and all animals in
-tough situations. Please post, cross post and share, if we all do our part
+tough situations.
+                </h4>
+                <h4>
+Please post, cross post and share, if we all do our part
 we can be the voice that they don't have and help them find their happily
 ever afters.
                 </h4>
