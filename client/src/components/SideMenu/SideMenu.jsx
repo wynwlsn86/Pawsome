8a793667249d32933a4f3e98659308ccb2536f59
catvonnegut <mulvaneytara@gmail.com>
2019-09-11 22:29:45 -0400

    sidemenu functionality working

--- client/src/components/SideMenu/SideMenu.jsx
+++ client/src/components/SideMenu/SideMenu.jsx
@@ -9,20 +9,56 @@ import './SideMenu.css';
 const sideMenu = props => (
   <nav className="side-menu">
     <ul>
-      <Link to='/find-a-pet'>
-        <li>Find a Pet</li>
+      <Link
+        to='/find-a-pet'
+        onClick={props.drawerClickHandler}>
+        <li>
+        <>&nbsp;</>
+        Find a Pet
+        <>&nbsp;</>
+        </li>
       </Link>
-      <Link to='/volunteers'>
+      <Link
+        to='/volunteers'
+        onClick={props.drawerClickHandler}>
         <li>Volunteer</li>
       </Link>
-      <Link to='/add-pet'>
-        <li>Add Pet</li>
+      <Link
+        to='/add-pet'
+        onClick={props.drawerClickHandler}>
+        <li>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        Add Pet
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        </li>
       </Link>
-      <Link to='/contact-us'>
+      <Link
+        to='/contact-us'
+        onClick={props.drawerClickHandler}>
         <li>Contact Us</li>
       </Link>
-      <Link to='/login'>
-        <li>Login</li>
+      <Link
+        to='/login'
+        onClick={props.drawerClickHandler}>
+        <li>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        Login
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        <>&nbsp;</>
+        </li>
       </Link>
     </ul>
   </nav>
